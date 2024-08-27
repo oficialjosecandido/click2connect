93d084726c5c877876ad5fc920e1d065b984f331
JOSE CANDIDO <jose.candido_ext@a-to-be.com>
2024-08-27 14:36:43 +0100

    firebase not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AngularFireModule } from "@angular/fire/compat";
+import { environment } from '../environments/environment';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
   ],
   providers: [
     provideClientHydration()
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBkEG1_NZM8g-5BJiU0v9irUy-UXBB2cPI",
+        authDomain: "consulting-a6c68.firebaseapp.com",
+        databaseURL: "https://consulting-a6c68-default-rtdb.firebaseio.com",
+        projectId: "consulting-a6c68",
+        storageBucket: "consulting-a6c68.appspot.com",
+        messagingSenderId: "8955803533",
+        appId: "1:8955803533:web:cf626942121ccdb259afc4",
+        measurementId: "G-CLN74X6CK6"
+    },
+    stripeKey: "pk_test_51PGBE0Ci4mI52FHufySY3hMMzT4VGwzVKDtRejF3dDDCoBDOJ0sh9pW7BOrq957iLh1LLEK3MSRwZts4xXItKxVe00tL21v1hf",
+  };
